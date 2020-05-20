a0e00d3e041f7c99e491a88de8e9e8370ae42642
bbddeleccionesgenerales <62172055+bbddeleccionesgenerales@users.noreply.github.com>
2020-05-20 22:17:29 +0200

    Manu

--- Tablas individuales/eleccions_municipis.sql
+++ Tablas individuales/eleccions_municipis.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `eleccions_municipis` (
   `eleccio_municipi_id` INT(10) UNSIGNED AUTO_INCREMENT NOT NULL,
   `eleccio_id` TINYINT(3) UNSIGNED NOT NULL,
-  `codi_ine_municipi`CHAR(3) NULL DEFAULT NULL,
+  `municipi_id`SMALLINT(5) UNSIGNED NOT NULL,
   `num_meses` SMALLINT(5) UNSIGNED NULL DEFAULT NULL,
   `poblacio` INT(10) UNSIGNED NULL DEFAULT NULL,
   `cens` INT(10) UNSIGNED NULL DEFAULT NULL,
@@ -11,6 +11,6 @@ CREATE TABLE `eleccions_municipis` (
   `vots_blanc` INT(10) UNSIGNED NULL DEFAULT NULL,
   `vots_nuls` INT(10) UNSIGNED NULL DEFAULT NULL,
   CONSTRAINT PK_eleccio_municipi_id PRIMARY KEY (`eleccio_municipi_id`),
-  CONSTRAINT FK_eleccions_municipis_municipis FOREIGN KEY (codi_ine_minicipi)
-    REFERENCES municipis (codi_ine_minicipi)
+  CONSTRAINT FK_eleccions_municipis_municipis FOREIGN KEY (municipi_id)
+    REFERENCES municipis (municipi_id)
 );
